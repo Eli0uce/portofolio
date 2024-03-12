853cd33ed4652eb7b5d54acc01520e1e218f0103
Eli0uce <eliouce.gaming@outlook.fr>
2024-03-13 09:24:42 +1100

    updateCursor

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 document.getElementById("nav-toggle").onclick = function () {
   document.getElementById("nav-content").classList.toggle("hidden");
 };
+
+new kursor({
+  type: 1,
+  color: "#fff",
+  removeDefaultCursor: true,
+});
